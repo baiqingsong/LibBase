9456a389a2cb7f03ca752df0e1ec63c84845140b
Bai Qing Song <904494377@qq.com>
2023-07-08 17:16:19 +0800

    基础类的引用

--- app/src/main/java/com/dawn/libbase/MainActivity.java
+++ app/src/main/java/com/dawn/libbase/MainActivity.java
@@ -3,6 +3,9 @@ package com.dawn.libbase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.dawn.library.util.LImageUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView ivLogo = findViewById(R.id.iv_logo);
+        LImageUtil.displayImage(this, "https://camera-jargee.jargee.cn/img/1E0E5266886600000000/photo_combine_b7132d75-3d6f-418b-89bf-b8a0c918a723.png", ivLogo);
+
     }
 }
